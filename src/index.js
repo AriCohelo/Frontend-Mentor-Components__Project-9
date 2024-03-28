1158fe7f747e4fcb331579cd31a7e173ce64db08
AriCohelo <ariel.cohelo@gmail.com>
2024-03-28 03:29:28 +0100

    finished the 9nth project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.scss';
+const shareButton = document.querySelector('.author__share-button');
+const shareBox = document.querySelector('.author__share-box');
+const arrow = document.querySelector('.author__share-button-arrow');
+
+shareButton.addEventListener('click', () => {
+	shareBox.classList.toggle('author__share-box--active-desktop');
+	shareButton.classList.toggle('author__share-box--active-button');
+	arrow.classList.toggle('author__share-box--active-button');
+});
